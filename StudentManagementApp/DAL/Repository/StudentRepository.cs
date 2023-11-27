7493c3292da7855085755a4a615b33999088dab7
IIHT Developers <93914497+IIHTDevelopers@users.noreply.github.com>
2023-11-27 13:25:44 +0530

    Add files via upload

---
+++ StudentManagementApp/DAL/Repository/StudentRepository.cs
@@ -0,0 +1,50 @@
+﻿using StudentManagementApp.DAL.Interface;
+using StudentManagementApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace StudentManagementApp.DAL.Repository
+{
+    public class StudentRepository : IStudentRepository
+    {
+        private StudentDbContext _context;
+        public StudentRepository(StudentDbContext Context)
+        {
+            this._context = Context;
+        }
+      
+        public Student GetStudentById(int studentId)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Student> GetAllStudents()
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public Student AddStudent(Student student)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+
+        public Student UpdateStudent(Student updatedStudent)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+
+        public Student DeleteStudent(int studentId)
+        {
+            //write your code here
+            throw new NotImplementedException();
+        }
+    }
+}
